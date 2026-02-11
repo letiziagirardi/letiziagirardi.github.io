fa94ca26480642c8b83d179d1350ef7b98e32c2a
letiziagirardi <71395970+letiziagirardi@users.noreply.github.com>
2026-02-11 21:45:50 +0000

    Deploying to gh-pages from @ letiziagirardi/letiziagirardi.github.io@b81226dfd42eef3d09fb7e48d2be83b71e590576 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,20 +481,6 @@ ninja.data = [{
           window.open("mailto:%6C%65%74%69%7A%69%61.%67%30%30@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("//feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
